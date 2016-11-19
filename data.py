487fd6cadb0e8a039535aa09736dfd6826242b93
Brian Shimanuki <bshimanuki@mit.edu>
2016-11-19 04:23:07 -0500

    word2vec which trains on all of the bible sentences Has loss function. Normalized based on word frequncies. We may want to eventually change this as predicted frequencies should be based on style. Also may worry about training word vectors on same data as the VAE.

--- data.py
+++ data.py
@@ -1,12 +1,42 @@
+import itertools
+import random
+import pickle
+import re
+
 import nltk
 import numpy as np
-from gensim.models import Word2Vec
 
-wd = 50
-word2vec = Word2Vec(map(lambda s: map(str.lower, s),
-                            nltk.corpus.brown.sents() +
-			    nltk.corpus.gutenberg.sents('bible-kjv.txt')),
-                            size=wd)
+from word2vec import Word2Vec
+
+OUTPUT = 'output'
+
+bible_path = 'data/bible/cache'
+bible_books = ['Genesis', 'Exodus', 'Leviticus', 'Numbers', 'Deuteronomy', 'Joshua', 'Judges', 'Ruth', '1 Samuel', '2 Samuel', '1 Kings', '2 Kings', '1 Chronicles', '2 Chronicles', 'Ezra', 'Nehemiah', 'Esther', 'Job', 'Psalms', 'Proverbs', 'Ecclesiastes', 'Song of Songs', 'Isaiah', 'Jeremiah', 'Lamentations', 'Ezekiel', 'Daniel', 'Hosea', 'Joel', 'Amos', 'Obadiah', 'Jonah', 'Micah', 'Nahum', 'Habakkuk', 'Zephaniah', 'Haggai', 'Zechariah', 'Malachi', 'Matthew', 'Mark', 'Luke', 'John', 'Acts', 'Romans', '1 Corinthians', '2 Corinthians', 'Galatians', 'Ephesians', 'Philippians', 'Colossians', '1 Thessalonians', '2 Thessalonians', '1 Timothy', '2 Timothy', 'Titus', 'Philemon', 'Hebrews', 'James', '1 Peter', '2 Peter', '1 John', '2 John', '3 John', 'Jude', 'Revelation']
+
+def process_sentence(sentence):
+    sentence = re.sub(r'(\W+)', r' \1 ', sentence)
+    return [w.lower() for w in sentence.split()]
+
+def process_book(name):
+    sents = []
+    with open('%s/%s.pickle' % (bible_path, name), 'rb') as f:
+        book = pickle.load(f)
+        for _, chapter in sorted(book.items()):
+            for _, verse in sorted(chapter.items()):
+                for _, trans in sorted(verse.items()):
+                    if trans is not None:
+                        sents.append(process_sentence(trans))
+    return sents
+
+def process_bible():
+    def shuffled_book(book):
+        text = process_book(book)
+        random.shuffle(text)
+        return text
+    class Iterator:
+        def __iter__(self):
+            return itertools.chain(*map(shuffled_book, bible_books))
+    return Iterator()
 
 def np_from_sentences(sentences):
     # TODO: Handle punctuation
@@ -16,3 +46,15 @@ def np_from_sentences(sentences):
     for i, sent in enumerate(v):
         out[i, :lens[i]] = np.array(sent)
     return out, lens
+
+wd = 50
+try:
+    with open(OUTPUT + '/word2vec.pickle', 'rb') as f:
+        word2vec = pickle.load(f)
+except FileNotFoundError:
+    word2vec = Word2Vec(process_bible(),
+            size=wd)
+    word2vec.normalize()
+    with open(OUTPUT + '/word2vec.pickle', 'wb') as f:
+        pickle.dump(word2vec, f)
+
--- word2vec.py
+++ word2vec.py
@@ -1,13 +1,44 @@
-import nltk
-from gensim.models.word2vec import Word2Vec
+import itertools
 
-WV_SIZE = 50
-sents = nltk.corpus.brown.sents() + \
-      nltk.corpus.gutenberg.sents('bible-kjv.txt')
-sents = [[w.lower() for w in s] for s in sents]
-word2vec = Word2Vec(sents,
-        size=WV_SIZE)
+import gensim
+import nltk
+import numpy as np
+import tensorflow as tf
+
+class Word2Vec(gensim.models.word2vec.Word2Vec):
+    def __init__(self, *args, **kwargs):
+        kwargs['null_word'] = True
+        super().__init__(*args, **kwargs)
+        self.null_word = '\0' # from internal gensim model
+
+    # We normalize based on word frequencies
+    def normalize(self):
+        # null_word does not have freq counts, so set one before normalization
+        self.vocab[self.null_word].count = self.min_count
+        self.init_sims(replace=True)
+        counts = np.zeros(len(self.vocab))
+        for word in self.vocab.values():
+            counts[word.index] = word.count
+        counts = counts.reshape((-1,1))
+        # log because softmax will recover the frequency proportions
+        self.syn0 *= np.log(counts)
+
+    def loss(self, vector, word):
+        dist = tf.matmul(self.syn0, tf.reshape(vector, (-1,1)))
+        dist = tf.transpose(dist)
+        return tf.nn.sparse_softmax_cross_entropy_with_logits(dist, [self.vocab[word].index])
+
+# corpus = [
+        # nltk.corpus.brown.sents(),
+        # nltk.corpus.gutenberg.sents('bible-kjv.txt'),
+        # ]
+
+# WV_SIZE = 50
+# sents = [[w.lower() for w in s] for s in itertools.chain(*corpus)]
+# word2vec = Word2Vec(sents,
+        # size=WV_SIZE)
+# word2vec.normalize()
 
 # examples
-print(word2vec['university'])
-print(word2vec.most_similar('peter'))
+# print(word2vec['university'])
+# print(word2vec.most_similar('peter'))
