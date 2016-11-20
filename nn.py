5f0f17cc30a2ebeab13f96f21ebcadb3cb3b9ea9
George Du <georgedu94@gmail.com>
2016-11-20 03:17:29 -0500

    Added loss function with several TODOs

--- nn.py
+++ nn.py
@@ -3,14 +3,14 @@ import numpy as np
 import data
 
 batch_size = 10
-lstm_size = 10
+lstm_size = 50 # TODO: allow lstm_size to be different from num_features
 max_steps = 100
 latent_dim = 2 * lstm_size
 embedding_np = data.get_embedding()
 num_words, num_features = embedding_np.shape
 
-# TODO: choose eos_matrix in a smart way
-eos_matrix = tf.constant(np.zeros((batch_size, 1, num_features)), dtype=tf.float32)
+# TODO: Check that this is a trainable parameter
+eos_matrix = tf.Variable(np.zeros((batch_size, 1, num_features)), dtype=tf.float32)
 
 # Placeholder for the inputs in a given iteration
 words = tf.placeholder(tf.int32, [batch_size, None])
@@ -26,6 +26,7 @@ embedding_init = embedding_matrix.assign(embedding_placeholder)
 
 word_vectors = tf.nn.embedding_lookup([embedding_matrix], words)
 eos_plus_words = tf.concat(1, [eos_matrix, word_vectors])
+#words_plus_eos = tf.concat(1, [word_vectors, eos_matrix])
 
 with tf.variable_scope('encoder'):
     encoder = tf.nn.rnn_cell.LSTMCell(num_units=lstm_size, state_is_tuple=False)
@@ -65,6 +66,24 @@ with tf.variable_scope('decoder'):
     # Get output and last hidden state
     outputs, _ = tf.nn.dynamic_rnn(decoder, eos_plus_words, sequence_length=lens_plus_one, dtype=tf.float32)
 
+# Compute probabilities
+# TODO: outputs contains eos, but one_hot doesn't include eos
+outputs_reshaped = tf.reshape(outputs, [-1, num_features])
+raw_probs = tf.matmul(outputs_reshaped, embedding_matrix, transpose_b=True)
+logits = tf.reshape(raw_probs, [batch_size, -1, num_words])
+log_probs = tf.nn.log_softmax(logits)
+one_hot = tf.one_hot(words, num_words)
+relevant_log_probs = tf.reduce_sum(tf.mul(log_probs, one_hot), 2)
+
+# Initialize loss to KLD
+loss = -0.5 * tf.reduce_sum(1 + logvar_encoder - tf.pow(mu_encoder, 2) - tf.exp(logvar_encoder), reduction_indices=1)
+# TODO: Check that using lens for indexing is valid
+for i in range(batch_size):
+    loss = loss - tf.reduce_sum(relevant_log_probs[i,:lens[i]])
+loss = loss / 2.0 # Normalization constant for mean
+train_step = tf.train.AdamOptimizer(0.001).minimize(loss)
+
+# Execute some test code
 with open('test_data.txt', 'r') as f:
     sentences = f.readlines()
 
@@ -73,5 +92,4 @@ sentences, lengths = data.word_indices(sentences)
 with tf.Session() as sess:
     sess.run(tf.initialize_all_variables())
     sess.run(embedding_init, feed_dict={embedding_placeholder:embedding_np})
-    out = sess.run(outputs, feed_dict={words:sentences, lens:lengths})
-    print(out)
+    sess.run(train_step, feed_dict={words:sentences, lens:lengths})
