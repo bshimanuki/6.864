c26e7ba7b7ef912233b4efd25e2b8a04680b691a
Brian Shimanuki <bshimanuki@mit.edu>
2016-11-25 18:21:18 -0500

    filtering simple sentences from NIRV

---
+++ nirv.py
@@ -0,0 +1,27 @@
+from collections import Counter
+
+from data.bible.training_data import read_bible
+from embedding import word2vec
+
+nirv = read_bible()['NIRV']
+sents = []
+s = []
+for sent in nirv:
+    for w in sent.split():
+        s.append(w)
+        if w in '.?!':
+            sents.append(s)
+            s = []
+
+VOCAB_THRESH = 500 # over all translations
+SENT_LEN = 20
+
+common = list(filter(lambda x: all(map(lambda y:y in word2vec and word2vec.vocab[y].count >= VOCAB_THRESH, x)), sents))
+
+# print(sorted(common, key=len)[-1])
+
+short = list(filter(lambda x: len(x) <= SENT_LEN, common))
+
+# print('Vocab size:', len(set([w for s in short for w in s])))
+# print('Sentence lengths:', Counter(map(len, short)))
+# print(len(sents), len(common), len(short))
