d309da7b4e6be03af78ad09c75b411ba1fc882a0
Jingyu Li <6.170-yuta@mit.edu>
2016-12-02 23:40:22 -0500

    one hot embedding

--- embedding.py
+++ embedding.py
@@ -73,6 +73,25 @@ def word_indices(sentences, eos=False):
     v = np.stack([word2vec.words_to_indices(sent, sent_length=max_len) for sent in sents], axis=0)
     return v, lens
 
+def one_hot_indices(sentences, eos=False):
+    sents = [process_sentence(sent, eos) for sent in sentences]
+    if eos:
+        lens = np.array(len(sent) - 1 for sent in sentences]
+    else:
+        lens = np.array(len(sent) for sent in sentences]
+    max_len = max(lens)
+    embeddings = []
+    for sent in sentences:
+        one_hot_sentence = word2vec.one_hot(sent, max_len)
+        embeddings.append(one_hot_sentence)
+    return np.array(embeddings), lens
+
+def get_one_hot_matrix():
+    vocab = word2vec.index_to_word_map()
+    num_words = len(vocab)
+    identity = np.identity((num_words, num_words))
+    return identity
+
 def get_embedding_matrix():
     return word2vec.embedding_matrix()
 
@@ -85,6 +104,11 @@ def embedding_to_sentence(embeddings):
     num_words_sentence, num_words_vocab = word_probs.shape
     word_sequence = [vocab[np.argmax(word_probs[i])] for i in range(num_words_sentence)]
     return ' '.join(word_sequence)
+
+def one_hot_to_sentence(embeddings):
+    vocab = word2vec.index_to_word_map()
+    word_indices = [word_vec.index(max(word_vec)) for word_vec in embeddings]
+    return ' '.join([vocab[index] for index in word_indices]
 
 wd = 200
 try:
