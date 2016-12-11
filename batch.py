11134e10de6c2392dffe454801ae4670f6ed5407
Brian Shimanuki <bshimanuki@mit.edu>
2016-12-11 17:12:49 -0500

    batch quads for 2 translations

--- batch.py
+++ batch.py
@@ -51,3 +51,33 @@ class Pairs:
 
     def num_training(self):
         return len(self.a)
+
+class Quads:
+    """Use for a pair corpus only. Will need to rewrite for multiple translations."""
+    def __init__(self, pair_corpus, train_ratio=0.9):
+        a = list(pair_corpus)
+        random.seed(1337)
+        random.shuffle(a)
+        t = int(train_ratio * len(a))
+        self.a = a[:t]
+        self.b = a[t:]
+        self.i = 0
+
+    def next_batch(self, batch_size):
+        if self.i + 2*batch_size >= len(self.a):
+            self.i = 0
+            random.shuffle(self.a)
+        out = [list(t) for t in zip(*self.a[self.i:self.i+batch_size])]
+        out2 = [list(t) for t in zip(*self.a[self.i+batch_size:self.i+2*batch_size])]
+        self.i += 2*batch_size
+        print(out)
+        return out[0], out[1], out2[0], out2[1]
+
+    def random_validation_batch(self, batch_size):
+        random.shuffle(self.b)
+        out = [list(t) for t in zip(*self.a[:batch_size])]
+        out2 = [list(t) for t in zip(*self.a[batch_size:2*batch_size])]
+        return out[0], out[1], out2[0], out2[1]
+
+    def num_training(self):
+        return len(self.a) / 2
