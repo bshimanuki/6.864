780dc1acb6e18ed8ac7a250730a79363792a4ef6
George Du <georgedu94@gmail.com>
2017-01-31 20:57:50 -0800

    classifier.py: code cleanup and add normalization

--- classifier.py
+++ classifier.py
@@ -3,6 +3,7 @@ from scipy import sparse
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 
 from constants import TRAIN_RATIO, VALIDATION_RATIO, TEST_RATIO, RANDOM_SEED
 from data.bible.training_data import get_corresponding_sentences_in_bible as get_pairs
@@ -17,11 +18,11 @@ from pair_nn import get_hidden
 # trans_pairs = get_pairs('ASV', 'CEB')
 
 # trans_pairs = get_corresponding_sentences_in_bible_multiple(['ASV', 'CEB', 'WYC', 'WEB'])
-use_svm = False
 
+use_svm = True
 def _get_classifier(c):
     if use_svm:
-        return SVC(C=c, kernel='rbf')
+        return SVC(C=c, kernel='rbf', gamma=0.005)
     else:
         return LogisticRegression(C=c)
 
@@ -36,6 +37,31 @@ def get_corpus(trans1, trans2):
                 vocab.add(word)
 
     return trans_pairs, vocab
+
+def perform_split(feature_vectors, labels):
+    train_val_feature_vectors, test_feature_vectors, train_val_labels, test_labels = train_test_split(
+        feature_vectors, labels, test_size=TEST_RATIO, random_state=RANDOM_SEED
+    )
+
+    scaler = StandardScaler().fit(train_val_feature_vectors)
+    train_val_feature_vectors = scaler.transform(train_val_feature_vectors)
+    test_feature_vectors = scaler.transform(test_feature_vectors)
+
+    test_feature_vectors = sparse.csr_matrix(test_feature_vectors)
+
+    train_feature_vectors, validation_feature_vectors, train_labels, validation_labels = train_test_split(
+        train_val_feature_vectors, train_val_labels, test_size=(VALIDATION_RATIO)/(VALIDATION_RATIO+TRAIN_RATIO), random_state=RANDOM_SEED
+    )
+
+    train_val_feature_vectors = sparse.csr_matrix(train_val_feature_vectors)
+    train_feature_vectors = sparse.csr_matrix(train_feature_vectors)
+    validation_feature_vectors = sparse.csr_matrix(validation_feature_vectors)
+
+    print("Split dataset into training, test and validation sets.")
+    print("Training size: {}".format(np.shape(train_feature_vectors)))
+    print("Validation size: {}".format(np.shape(validation_feature_vectors)))
+    print("Test size: {}".format(np.shape(test_feature_vectors)))
+    return train_feature_vectors, validation_feature_vectors, train_val_feature_vectors, test_feature_vectors, train_labels, validation_labels, train_val_labels, test_labels
 
 def get_unigram_features(trans1, trans2, use_unk=True):
     trans_pairs, vocab = get_corpus(trans1, trans2)
@@ -65,27 +91,10 @@ def get_unigram_features(trans1, trans2, use_unk=True):
     print("Converted sentences to feature vectors and labels.")
     print("\tLabels: {}".format(np.array(labels)))
 
-    train_val_feature_vectors, test_feature_vectors, train_val_labels, test_labels = train_test_split(
-        feature_vectors, labels, test_size=TEST_RATIO, random_state=RANDOM_SEED
-    )
-
+    ret = perform_split(feature_vectors, labels)
     del feature_vectors
     del labels
-    test_feature_vectors = sparse.csr_matrix(test_feature_vectors)
-
-    train_feature_vectors, validation_feature_vectors, train_labels, validation_labels = train_test_split(
-        train_val_feature_vectors, train_val_labels, test_size=(VALIDATION_RATIO)/(VALIDATION_RATIO+TRAIN_RATIO), random_state=RANDOM_SEED
-    )
-
-    train_val_feature_vectors = sparse.csr_matrix(train_val_feature_vectors)
-    train_feature_vectors = sparse.csr_matrix(train_feature_vectors)
-    validation_feature_vectors = sparse.csr_matrix(validation_feature_vectors)
-
-    print("Split dataset into training, test and validation sets.")
-    print("Training size: {}".format(np.shape(train_feature_vectors)))
-    print("Validation size: {}".format(np.shape(validation_feature_vectors)))
-    print("Test size: {}".format(np.shape(test_feature_vectors)))
-    return train_feature_vectors, validation_feature_vectors, train_val_feature_vectors, test_feature_vectors, train_labels, validation_labels, train_val_labels, test_labels
+    return ret
 
 def get_nn_features():
     features1, features2 = get_hidden(["style", "logvar_style"])
@@ -101,27 +110,10 @@ def get_nn_features():
     print("Converted sentences to feature vectors and labels.")
     print("\tLabels: {}".format(np.array(labels)))
 
-    train_val_feature_vectors, test_feature_vectors, train_val_labels, test_labels = train_test_split(
-        feature_vectors, labels, test_size=TEST_RATIO, random_state=RANDOM_SEED
-    )
-
+    ret = perform_split(feature_vectors, labels)
     del feature_vectors
     del labels
-    test_feature_vectors = sparse.csr_matrix(test_feature_vectors)
-
-    train_feature_vectors, validation_feature_vectors, train_labels, validation_labels = train_test_split(
-        train_val_feature_vectors, train_val_labels, test_size=(VALIDATION_RATIO)/(VALIDATION_RATIO+TRAIN_RATIO), random_state=RANDOM_SEED
-    )
-
-    train_val_feature_vectors = sparse.csr_matrix(train_val_feature_vectors)
-    train_feature_vectors = sparse.csr_matrix(train_feature_vectors)
-    validation_feature_vectors = sparse.csr_matrix(validation_feature_vectors)
-
-    print("Split dataset into training, test and validation sets.")
-    print("Training size: {}".format(np.shape(train_feature_vectors)))
-    print("Validation size: {}".format(np.shape(validation_feature_vectors)))
-    print("Test size: {}".format(np.shape(test_feature_vectors)))
-    return train_feature_vectors, validation_feature_vectors, train_val_feature_vectors, test_feature_vectors, train_labels, validation_labels, train_val_labels, test_labels
+    return ret
 
 def check_matches(labels, predicted_labels):
     if np.size(labels) != np.size(predicted_labels):
@@ -139,7 +131,7 @@ def evaluate_pairs(trans1, trans2, type='unigram'):
 
 def process(train_feature_vectors, validation_feature_vectors, train_val_feature_vectors, test_feature_vectors, train_labels, validation_labels, train_val_labels, test_labels):
     print("Using validation set to optimize over value of regularization parameter in regression, C.")
-    best_proportion_matched = 0
+    best_acc = 0
     C_RANGE = [0.1, 1, 10]
     for C_cur in C_RANGE:
         """
@@ -150,21 +142,23 @@ def process(train_feature_vectors, validation_feature_vectors, train_val_feature
         print("\tCurrent C: {}".format(C_cur))
         classifier = _get_classifier(C_cur)
         classifier.fit(train_feature_vectors, train_labels)
+        predicted_train_labels = classifier.predict(train_feature_vectors)
         predicted_validation_labels = classifier.predict(validation_feature_vectors)
-        (num_matches, proportion_matched) = check_matches(validation_labels, predicted_validation_labels)
-        print("\t\tNumber of matches: {}".format(num_matches))
-        print("\t\tProportion of matches: {}".format(proportion_matched))
-        if proportion_matched > best_proportion_matched:
+        (_, train_acc) = check_matches(train_labels, predicted_train_labels)
+        (_, val_acc) = check_matches(validation_labels, predicted_validation_labels)
+        print("\t\tTraining accuracy: {}".format(train_acc))
+        print("\t\tValidation accuracy: {}".format(val_acc))
+        if val_acc > best_acc:
             best_C = C_cur
-            best_proportion_matched = proportion_matched
+            best_acc = val_acc
 
-        print("Done training and validating. Best C found: {}, Best accuracy on validation: {}".format(best_C, best_proportion_matched))
+    print("Done training and validating. Best C found: {}, Best accuracy on validation: {}".format(best_C, best_acc))
 
     classifier = _get_classifier(best_C)
     classifier.fit(train_val_feature_vectors, train_val_labels)
-    predicted_train_labels = classifier.predict(train_feature_vectors)
+    predicted_train_val_labels = classifier.predict(train_val_feature_vectors)
     predicted_test_labels = classifier.predict(test_feature_vectors)
-    (num_matches_train, accuracy_train) = check_matches(train_labels, predicted_train_labels)
+    (num_matches_train, accuracy_train) = check_matches(train_val_labels, predicted_train_val_labels)
     (num_matches_test, accuracy_test) = check_matches(test_labels, predicted_test_labels)
     print("Accuracy on train {}".format(accuracy_train))
     print("Accuracy on test {}".format(accuracy_test))
